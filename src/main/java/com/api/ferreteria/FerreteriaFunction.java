7f712889dddfacf5619a4e98e9fec6c3ea96b358
cesarg.lineros <cesarg.lineros@gmail.com>
2025-06-30 03:43:50 +0000

    integración de cloud firebase

---
+++ src/main/java/com/api/ferreteria/FerreteriaFunction.java
@@ -0,0 +1,9 @@
+package com.api.ferreteria;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootApiGatewayRequestHandler;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerreteriaFunction extends SpringBootApiGatewayRequestHandler {
+
+}
